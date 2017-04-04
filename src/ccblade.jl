9ebdd5668f5771b61995107b32b7c6104edb7915
Andrew Ning <aning@byu.edu>
2017-04-04 13:07:29 -0600

    various cleanup/reorg

--- src/ccblade.jl
+++ src/ccblade.jl
@@ -10,8 +10,6 @@ Allows for non-ideal conditions (reversed flow, no wind in one direction, etc.)
 
 module BEM
 
-using PyPlot  # TODO: remove
-
 using Roots: fzero
 using Dierckx: Spline1D
 
@@ -41,8 +39,8 @@ immutable Rotor
     chord::Array{Float64, 1}
     theta::Array{Float64, 1}
     af::Array{AirfoilData, 1}
-    Vx::Array{Float64, 1}
-    Vy::Array{Float64, 1}
+    # Vx::Array{Float64, 1}
+    # Vy::Array{Float64, 1}
     Rhub::Float64
     Rtip::Float64
     B::Int64
@@ -50,15 +48,21 @@ end
 
 # operating point for the turbine/propeller
 immutable OperatingPoint
-    Vinf::Float64
-    Omega::Float64
-    pitch::Float64
-    azimuth::Float64
+    # Vinf::Float64
+    # Omega::Float64
+    # pitch::Float64
+    # azimuth::Float64
+    Vx::Array{Float64, 1}
+    Vy::Array{Float64, 1}
     rho::Float64
 end
 
+
 function readaerodyn(filename)
-    """currently only reads one Reynolds number if multiple exist"""
+    """
+    read af files in aerodyn format.
+    currently only reads one Reynolds number if multiple exist
+    """
 
     alpha = Float64[]
     cl = Float64[]
@@ -95,22 +99,22 @@ function readaerodyn(filename)
 end
 
 
-function definecurvature(r, precone)
-
-    # coordinate in azimuthal coordinate system
-    x_az = -r*sin(precone) # + precurve*cos(precone)
-    z_az = r*cos(precone) # + precurve*sin(precone)
-    y_az = zeros(r) # presweep
-
-    # compute total coning angle for purposes of relative velocity
-    cone = zeros(r)
-    cone[1] = atan2(-(x_az[2] - x_az[1]), z_az[2] - z_az[1])
-    cone[2:end-1] = 0.5*(atan2(-(x_az[2:end-1] - x_az[1:end-2]), z_az[2:end-1] - z_az[1:end-2])
-                       + atan2(-(x_az[3:end] - x_az[2:end-1]), z_az[3:end] - z_az[2:end-1]))
-    cone[end] = atan2(-(x_az[end] - x_az[end-1]), z_az[end] - z_az[end-1])
-
-    return x_az, y_az, z_az, cone
-end
+# function definecurvature(r, precone)
+#
+#     # coordinate in azimuthal coordinate system
+#     x_az = -r*sin(precone) # + precurve*cos(precone)
+#     z_az = r*cos(precone) # + precurve*sin(precone)
+#     y_az = zeros(r) # presweep
+#
+#     # compute total coning angle for purposes of relative velocity
+#     cone = zeros(r)
+#     cone[1] = atan2(-(x_az[2] - x_az[1]), z_az[2] - z_az[1])
+#     cone[2:end-1] = 0.5*(atan2(-(x_az[2:end-1] - x_az[1:end-2]), z_az[2:end-1] - z_az[1:end-2])
+#                        + atan2(-(x_az[3:end] - x_az[2:end-1]), z_az[3:end] - z_az[2:end-1]))
+#     cone[end] = atan2(-(x_az[end] - x_az[end-1]), z_az[end] - z_az[end-1])
+#
+#     return x_az, y_az, z_az, cone
+# end
 
 
 function windComponents(r, precone, yaw, tilt, hubHt, shearExp, op::OperatingPoint)
@@ -150,156 +154,15 @@ end
 
 
 
-function firstbracket(f, xmin, xmax, n, backwardsearch=false)
-    """Find a bracket for the root closest to xmin by subdividing
-    interval (xmin, xmax) into n intervals
-
-    Returns found, xl, xu.
-    If found = true a bracket was found between (xl, xu)
-    """
-
-    xvec = linspace(xmin, xmax, n)
-    if backwardsearch  # start from xmax and work backwards
-        xvec = reverse(xvec)
-    end
-
-    fprev = f(xvec[1])
-    for i = 2:n
-        fnext = f(xvec[i])
-        if fprev*fnext < 0  # bracket found
-            if backwardsearch
-                return true, xvec[i], xvec[i-1]
-            else
-                return true, xvec[i-1], xvec[i]
-            end
-        end
-        fprev = fnext
-    end
-
-    return false, 0.0, 0.0
-
-end
-
-
-function distributedLoads(rotor::Rotor, op::OperatingPoint)
-
-    # parameters
-    npts = 20
-
-    # quadrants
-    epsilon = 1e-6
-    q1 = [epsilon, pi/2]
-    q2 = [-pi/2, -epsilon]
-    q3 = [pi/2, pi-epsilon]
-    q4 = [-pi+epsilon, -pi/2]
-
-    # initialize arrays
-    n = length(rotor.r)
-    Np = zeros(n)
-    Tp = zeros(n)
-
-    for i = 1:n
-
-        # setup
-        twist = rotor.theta[i] + op.pitch
-        Vx = rotor.Vx[i]
-        Vy = rotor.Vy[i]
-        sec = Section(rotor.r[i], rotor.chord[i], twist, rotor.af[i], Vx, Vy,
-            rotor.Rhub, rotor.Rtip, rotor.B, rho)
-        startfrom90 = false
-
-        if isapprox(Vx, 0.0, rtol=1e-6) && isapprox(Vy, 0.0, rtol=1e-6)
-            Np[i] = 0.0; Tp[i] = 0.0
-            break
-
-        elseif isapprox(Vx, 0.0, rtol=1e-6)
-
-            resid = residualVx0
-
-            if Vy > 0 && twist > 0
-                order = (q1, q2)
-            elseif Vy > 0 && twist < 0
-                order = (q2, q1)
-            elseif Vy < 0 && twist > 0
-                order = (q3, q4)
-            else  # Vy < 0 && twist < 0
-                order = (q4, q3)
-            end
-
-        elseif isapprox(Vy, 0.0, rtol=1e-6)
-
-            resid = residualVy0
-            startfrom90 = true  # start from 90 deg
-
-            if Vx > 0 && abs(twist) < pi/2
-                order = (q1, q3)
-            elseif Vx < 0 && abs(twist) < pi/2
-                order = (q2, q4)
-            elseif Vx > 0 && abs(twist) > pi/2
-                order = (q3, q1)
-            else  # Vx < 0 && abs(twist) > pi/2
-                order = (q4, q2)
-            end
-
-        else  # normal case
-
-            resid = residual
-
-            if Vx > 0 && Vy > 0
-                order = (q1, q2, q3, q4)
-            elseif Vx < 0 && Vy > 0
-                order = (q2, q1, q4, q3)
-            elseif Vx > 0 && Vy < 0
-                order = (q3, q4, q1, q2)
-            else  # Vx < 0 && Vy < 0
-                order = (q4, q3, q2, q1)
-            end
-
-        end
-
-        function R(phi)
-            zero, _, _ = resid(phi, sec)
-            return zero
-        end
-
-        success = false
-        for j = 1:length(order)
-            phimin = order[j][1]
-            phimax = order[j][2]
-
-            # optimize search direction
-            backwardsearch = false
-            if !startfrom90
-                if phimin == -pi/2 || phimax == -pi/2  # q2 or q4
-                    backwardsearch = true
-                end
-            else
-                if phimax == pi/2  # q1
-                    backwardsearch = true
-                end
-            end
-
-            success, phiL, phiU = firstbracket(R, phimin, phimax, npts, backwardsearch)
-
-            if success
-
-                phistar = fzero(R, phiL, phiU)
-                _, Np[i], Tp[i] = resid(phistar, sec)
-
-                break
-            end
-
-            # if no solution found, it just returns zeros for this section, although this really shouldn't get here
-            # alternatively, one could increase npts and try again
-        end
-    end
-
-    return Np, Tp
-end
 
 
 function residualbase(phi::Float64, sec::Section)
+    """
+    residual function
+    base calculations used in normal case, Vx=0 case, and Vy=0 case
+    """
 
+    # unpack variables for convenience
     r = sec.r
     chord = sec.chord
     twist = sec.twist
@@ -341,10 +204,11 @@ function residualbase(phi::Float64, sec::Section)
     k = cn*sigma_p/(4.0*F*sphi*sphi)
     kp = ct*sigma_p/(4.0*F*sphi*cphi)
 
+    # parameters used in Vx=0 and Vy=0 cases
     k2 = cn*sigma_p/(4.0*F*cphi*cphi)
     kp2 = ct*sigma_p/(4.0*F*sphi*sphi)
 
-    # force for unit unit W
+    # force for unit W (because W varies between cases)
     Nunit = cn*0.5*rho*chord
     Tunit = ct*0.5*rho*chord
 
@@ -355,6 +219,7 @@ end
 
 
 function residual(phi::Float64, sec::Section)
+    """residual for normal case"""
 
     k, kp, F, _, _, Nu, Tu = residualbase(phi, sec)
     Vx = sec.Vx
@@ -412,6 +277,7 @@ end
 
 
 function residualVx0(phi::Float64, sec::Section)
+    """residual for Vx=0 case"""
 
     # base params
     _, _, _, k2, _, Nu, Tu = residualbase(phi, sec)
@@ -441,7 +307,9 @@ function residualVx0(phi::Float64, sec::Section)
     return R, Np, Tp
 end
 
+
 function residualVy0(phi::Float64, sec::Section)
+    """residual for Vy=0 case"""
 
     # base params
     _, _, _, _, kp2, Nu, Tu = residualbase(phi, sec)
@@ -463,8 +331,248 @@ end
 
 
 
+function firstbracket(f, xmin, xmax, n, backwardsearch=false)
+    """Find a bracket for the root closest to xmin by subdividing
+    interval (xmin, xmax) into n intervals
+
+    Returns found, xl, xu.
+    If found = true a bracket was found between (xl, xu)
+    """
+
+    xvec = linspace(xmin, xmax, n)
+    if backwardsearch  # start from xmax and work backwards
+        xvec = reverse(xvec)
+    end
+
+    fprev = f(xvec[1])
+    for i = 2:n
+        fnext = f(xvec[i])
+        if fprev*fnext < 0  # bracket found
+            if backwardsearch
+                return true, xvec[i], xvec[i-1]
+            else
+                return true, xvec[i-1], xvec[i]
+            end
+        end
+        fprev = fnext
+    end
+
+    return false, 0.0, 0.0
+
+end
 
 
+
+function distributedLoads(rotor::Rotor, op::OperatingPoint, turbine::Bool)
+    """
+    Compute the distributed loads along blade at specified condition
+    """
+
+    # check if propeller
+    swapsign = turbine ? 1 : -1
+
+    # parameters
+    npts = 20  # number of discretization points to find bracket in residual solve
+
+    # quadrants
+    epsilon = 1e-6
+    q1 = [epsilon, pi/2]
+    q2 = [-pi/2, -epsilon]
+    q3 = [pi/2, pi-epsilon]
+    q4 = [-pi+epsilon, -pi/2]
+
+    # initialize arrays
+    n = length(rotor.r)
+    Np = zeros(n)
+    Tp = zeros(n)
+
+    for i = 1:n  # iterate across blade
+
+        # setup
+        twist = swapsign*rotor.theta[i] #  + op.pitch  TODO: pitch is just part of theta specificiation
+        Vx = swapsign*op.Vx[i]
+        Vy = op.Vy[i]
+        sec = Section(rotor.r[i], rotor.chord[i], twist, rotor.af[i], Vx, Vy,
+            rotor.Rhub, rotor.Rtip, rotor.B, rho)
+        startfrom90 = false  # start bracket search from 90 deg instead of 0 deg.
+
+        # Vx = 0 and Vy = 0
+        if isapprox(Vx, 0.0, rtol=1e-6) && isapprox(Vy, 0.0, rtol=1e-6)
+            Np[i] = 0.0; Tp[i] = 0.0
+            break
+
+        # Vx = 0
+        elseif isapprox(Vx, 0.0, rtol=1e-6)
+
+            resid = residualVx0
+
+            if Vy > 0 && twist > 0
+                order = (q1, q2)
+            elseif Vy > 0 && twist < 0
+                order = (q2, q1)
+            elseif Vy < 0 && twist > 0
+                order = (q3, q4)
+            else  # Vy < 0 && twist < 0
+                order = (q4, q3)
+            end
+
+        # Vy = 0
+        elseif isapprox(Vy, 0.0, rtol=1e-6)
+
+            resid = residualVy0
+            startfrom90 = true  # start bracket search from 90 deg
+
+            if Vx > 0 && abs(twist) < pi/2
+                order = (q1, q3)
+            elseif Vx < 0 && abs(twist) < pi/2
+                order = (q2, q4)
+            elseif Vx > 0 && abs(twist) > pi/2
+                order = (q3, q1)
+            else  # Vx < 0 && abs(twist) > pi/2
+                order = (q4, q2)
+            end
+
+        else  # normal case
+
+            resid = residual
+
+            if Vx > 0 && Vy > 0
+                order = (q1, q2, q3, q4)
+            elseif Vx < 0 && Vy > 0
+                order = (q2, q1, q4, q3)
+            elseif Vx > 0 && Vy < 0
+                order = (q3, q4, q1, q2)
+            else  # Vx < 0 && Vy < 0
+                order = (q4, q3, q2, q1)
+            end
+
+        end
+
+        # wrapper to residual function to accomodate format required by fzero
+        function R(phi)
+            zero, _, _ = resid(phi, sec)
+            return zero
+        end
+
+        success = false
+        for j = 1:length(order)  # quadrant orders.  In most cases it should find root in first quadrant searched.
+            phimin = order[j][1]
+            phimax = order[j][2]
+
+            # check to see if it would be faster to reverse the bracket search direction
+            backwardsearch = false
+            if !startfrom90
+                if phimin == -pi/2 || phimax == -pi/2  # q2 or q4
+                    backwardsearch = true
+                end
+            else
+                if phimax == pi/2  # q1
+                    backwardsearch = true
+                end
+            end
+
+            # find bracket
+            success, phiL, phiU = firstbracket(R, phimin, phimax, npts, backwardsearch)
+
+            # once bracket is found, solve root finding problem and compute loads
+            if success
+
+                phistar = fzero(R, phiL, phiU)
+                _, Np[i], Tp[i] = resid(phistar, sec)
+
+                break
+            end
+
+            # if no solution found, it just returns zeros for this section, although this really shouldn't get here
+            # alternatively, one could increase npts and try again
+        end
+    end
+
+    return Np, Tp
+end
+
+
+
+
+function thrusttorque(Rhub, r, Rtip, precone, Np, Tp)
+    """integrate the thrust/torque across the blade, including 0 loads at hub/tip"""
+
+    # add hub/tip for complete integration.  loads go to zero at hub/tip.
+    rfull = [Rhub; r; Rtip]
+    Npfull = [0.0; Np; 0.0]
+    Tpfull = [0.0; Tp; 0.0]
+
+    # integrate Thrust and Torque (trapezoidal)
+    thrust = Npfull*cos(precone)
+    torque = Tpfull.*r*cos(precone)
+
+    T = trapz(rfull, thrust)
+    Q = trapz(rfull, torque)
+
+    return T, Q
+end
+
+
+function trapz(x::Array{Float64,1}, y::Array{Float64,1})  # integrate y w.r.t. x
+    """trapezoidal integration"""
+
+    integral = 0.0
+    for i = 1:length(x)-1
+        integral += (x[i+1]-x[i])*0.5*(y[i] + y[i+1])
+    end
+    return integral
+end
+
+
+function forces(rotor::Rotor, ops::Array{OperatingPoint, 1}, Omega::Float64, Vhub::Float64, turbine::Bool)  #, nsectors::Int64)
+    """one operating point for each sector"""
+
+    nsectors = length(ops)  # number of sectors (should be evenly spaced)
+    T = 0.0
+    Q = 0.0
+    rhovec = zeros(nsectors)
+
+    # coarse integration - rectangle rule
+    for j = 1:nsectors  # integrate across azimuth
+
+        Np, Tp = distributedLoads(rotor, ops[j], turbine)
+
+        Tsub, Qsub = thrusttorque(rotor.Rhub, rotor.r, rotor.Rtip, rotor.precone, Np, Tp)
+
+        T += rotor.B * Tsub / nsectors
+        Q += rotor.B * Qsub / nsectors
+
+        rhovec[i] = ops[j].rho
+    end
+
+    P = Q * Omega
+    Rp = rotor.Rtip*cos(rotor.precone)
+    Dp = 2*Rp
+
+    if turbine
+        q = 0.5 * rhovec * Vhub^2
+        A = pi * Rp^2
+        CP = P ./ (q * A * Vhub)
+        CT = T ./ (q * A)
+        CQ = Q ./ (q * Rp * A)
+
+        return CP, CT, CQ
+
+    else
+
+        n = Omega/(2*pi)
+
+        eff = T*Vhub/P
+        CT = T / (rho * n^2 * Dp^4)
+        CQ = Q / (rho * n^2 * Dp^5)
+
+        eff[T .< 0] = 0  # creating drag not thrust
+
+        return eff, CT, CQ
+
+    end
+
+end
 
 
 # geometry
