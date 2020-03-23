d4207223ef5546c5e75858e1e95f486cdf8644a4
Ingraham, Daniel James (GRC-LTV0) <daniel.j.ingraham@nasa.gov>
2020-03-23 13:57:59 -0400

    Small changes re: Andrew's comments

--- openmdao/src/CCBladeOpenMDAOExample.jl
+++ openmdao/src/CCBladeOpenMDAOExample.jl
@@ -1,5 +1,20 @@
 module CCBladeOpenMDAOExample
 
+# convenience function to set fields within an array of structs
+function Base.setproperty!(obj::Array{Rotor{TF, TI, TB, TF2}, N}, sym::Symbol, x) where {TF, TI, TB, TF2, N}
+    setfield!.(obj, sym, x)
+end
+
+# convenience function to set fields within an array of structs
+function Base.setproperty!(obj::Array{Section{TF1, TF2, TF3, TAF}, N}, sym::Symbol, x) where {TF1, TF2, TF3, TAF, N}
+    setfield!.(obj, sym, x)
+end
+
+# convenience function to set fields within an array of structs
+function Base.setproperty!(obj::Array{OperatingPoint{TF, TF2}, N}, sym::Symbol, x) where {TF, TF2, N}
+    setfield!.(obj, sym, x)
+end
+
 include("derivatives.jl")
 include("openmdao.jl")
 
--- src/CCBlade.jl
+++ src/CCBlade.jl
@@ -76,21 +76,10 @@ end
 # make rotor broadcastable as a single entity
 Base.Broadcast.broadcastable(r::Rotor) = Ref(r) 
 
-# convenience function to set fields within an array of structs
-function Base.setproperty!(obj::Array{Rotor{TF, TI, TB, TF2}, N}, sym::Symbol, x) where {TF, TI, TB, TF2, N}
-    setfield!.(obj, sym, x)
-end
-
 # convenience function to access fields within an array of structs
 function Base.getproperty(obj::Vector{Section{TF1, TF2, TF3, TAF}}, sym::Symbol) where {TF1, TF2, TF3, TAF}
     return getfield.(obj, sym)
 end
-
-# convenience function to set fields within an array of structs
-function Base.setproperty!(obj::Array{Section{TF1, TF2, TF3, TAF}, N}, sym::Symbol, x) where {TF1, TF2, TF3, TAF, N}
-    setfield!.(obj, sym, x)
-end
-
 
 """
     OperatingPoint(Vx, Vy, rho, mu=1.0, asound=1.0)
@@ -121,11 +110,6 @@ OperatingPoint(Vx, Vy, rho) = OperatingPoint(Vx, Vy, rho, one(rho), one(rho))
 # convenience function to access fields within an array of structs
 function Base.getproperty(obj::Array{OperatingPoint{TF, TF2}, N}, sym::Symbol) where {TF, TF2, N}
     return getfield.(obj, sym)
-end
-
-# convenience function to set fields within an array of structs
-function Base.setproperty!(obj::Array{OperatingPoint{TF, TF2}, N}, sym::Symbol, x) where {TF, TF2, N}
-    setfield!.(obj, sym, x)
 end
 
 
@@ -172,7 +156,7 @@ struct Outputs{TF}
 end
 
 # convenience constructor to initialize
-Outputs() = Outputs(0.0, 0.0, 0.0, 0.0, 0.1, 0.1, -5.0*pi/180.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+Outputs() = Outputs(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
 
 # convenience function to access fields within an array of structs
 function Base.getproperty(obj::Array{Outputs{TF}, N}, sym::Symbol) where {TF, N}
