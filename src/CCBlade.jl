eac5c061b69a2655363a9568e1fb4fbed7b71ab9
Andrew Ning <aning@byu.edu>
2020-05-06 11:33:53 -0600

    on the fly airfoil corrections

--- src/CCBlade.jl
+++ src/CCBlade.jl
@@ -24,6 +24,118 @@ export solve, thrusttorque, nondim
 
 include("airfoils.jl")  # all the code related to airfoil data
 
+# --------- Correction Methods -----------
+
+# -- Mach ---
+
+abstract type MachCorrection end
+
+struct NoMachCorrection <: MachCorrection end
+struct PrandtlGlauert <: MachCorrection end
+struct KarmanTsien <: MachCorrection end
+
+function machcorrection(::NoMachCorrection, cl, cd, Mach)
+    return cl, cd
+end
+
+function machcorrection(::PrantlGlauert, cl, cd, Mach)
+    beta = sqrt(1 - Mach^2)
+    cl /= beta
+    return cl, cd
+end
+
+function machcorrection(::KarmanTsien, cl, cd, Mach)
+    beta = sqrt(1 - Mach^2)
+    cl = 1.0/(beta/cl + Mach^2/(2*(1 + beta)))
+    return cl, cd
+end
+
+# -- Reynolds number ---
+
+abstract type ReCorrection end
+
+struct NoReCorrection <: ReCorrection end
+struct SkinFriction <: ReCorrection 
+    Re0::TF  # reference reynolds number
+    p::TF  # exponent.  ~0.2 fully turbulent (Schlichting), 0.5 fully laminar (Blasius)
+end
+
+function recorrection(::NoReCorrection, cl, cd, Re)
+    return cl, cd
+end
+
+function recorrection(sf::SkinFriction, cl, cd, Re)
+    cd *= (sf.Re0 / Re)^sf.p
+    return cl, cd
+end
+
+
+# -- Rotation ---
+
+abstract type RotationCorrection end
+
+struct NoRotationCorrection <: RotationCorrection
+struct DuSeligEggers <: RotationCorrection
+    a::TF
+    b::TF
+    d::TF
+end
+DuSeligEggers() = DuSeligEggers(1.0, 1.0, 1.0)
+
+function rotationcorrection(::NoRotationCorrection, cl, cd, cr, rR, tsr, alpha, phi)
+    return cl, cd
+end
+
+function rotationcorrection(du::DuSeligEggers, cl, cd, cr, rR, tsr, alpha, phi)
+    # Du-Selig correction for lift
+    Lambda = tsr / sqrt(1 + tsr^2)
+    expon = du.d / (Lambda * rR)
+    fcl = 1.0/(2*pi)*(1.6*cr/0.1267*(du.a-cr^expon)/(du.b+cr^expon)-1)
+    cl_linear = 2*pi*(alpha - alpha0)
+    deltacl = fcl*(cl_linear - cl)
+    cl += deltacl
+
+    # Eggers correction for drag
+    deltacd = deltacl*(sin(phi) - 0.12*cos(phi))/(cos(phi) + 0.12*sin(phi))  # note that we can actually use phi instead of alpha as is done in airfoilprep.py b/c this is done at each iteration
+    cd += deltacd
+
+    return cl, cd
+end    
+
+
+# --- tip correction  ---
+
+abstract type TipCorrection end 
+
+struct NoTipCorrection <: TipCorrection end
+struct PrandtlTipOnly <: TipCorrection end
+struct Prandtl <: TipCorrection end
+
+function tiplossfactor(::NoTipCorrection, r, Rhub, Rtip, phi, B)
+    return 1.0
+end
+
+function tiplossfactor(::PrandtlTipOnly, r, Rhub, Rtip, phi, B)
+    
+    asphi = abs(sin(phi))
+    factortip = B/2.0*(Rtip/r - 1)/asphi
+    F = 2.0/pi*acos(exp(-factortip))
+
+    return F
+end
+
+function tiplossfactor(::Prandtl, r, Rhub, Rtip, phi, B)
+
+    # Prandtl's tip and hub loss factor
+    asphi = abs(sin(phi))
+    factortip = B/2.0*(Rtip/r - 1)/asphi
+    Ftip = 2.0/pi*acos(exp(-factortip))
+    factorhub = B/2.0*(r/Rhub - 1)/asphi
+    Fhub = 2.0/pi*acos(exp(-factorhub))
+    F = Ftip * Fhub
+
+    return F
+end
 
 # --------- structs -------------
 
@@ -48,11 +160,16 @@ struct Rotor{TF, TI, TB}
     precone::TF
     flipcamber::TB
     negateoutputs::TB
-
+    MachC::MachCorrection
+    ReC::ReCorrection
+    rotationC::RotationCorrection
+    tipC::TipCorrection
 end
 
-# convenience constructor for no precone
-Rotor(Rhub, Rtip, B; precone=0.0, flipcamber=false, negateoutputs=false) = Rotor(Rhub, Rtip, B, precone, flipcamber, negateoutputs)
+# convenience constructor with keyword parameters
+Rotor(Rhub, Rtip, B; precone=0.0, flipcamber=false, negateoutputs=false, MachC=NoMachCorrection(), 
+    ReC=NoReCorrection(), rotationC=NoRotationCorrection(), tipC=Prandtl()) = Rotor(Rhub, Rtip, 
+    B, precone, flipcamber, negateoutputs, machC, ReC, rotationC, tipC)
 
 
 """
@@ -215,16 +332,17 @@ function residual(phi, rotor, section, op)
         cl, cd = af(alpha, Re, Mach)
     end
 
+    # airfoil corrections
+    cl, cd = machcorrection(rotor.MachC, cl, cd, Mach)
+    cl, cd = recorrection(rotor.ReC, cl, cd, Re)
+    cl, cd = rotationcorrection(rotor.rotationC, cl, cd, chord/r, r/Rtip, Vy/Vx*Rtip/r, alpha, phi)
+
     # resolve into normal and tangential forces
     cn = cl*cphi - cd*sphi
     ct = cl*sphi + cd*cphi
 
-    # Prandtl's tip and hub loss factor
-    factortip = B/2.0*(Rtip/r - 1)/abs(sphi)
-    Ftip = 2.0/pi*acos(exp(-factortip))
-    factorhub = B/2.0*(r/Rhub - 1)/abs(sphi)
-    Fhub = 2.0/pi*acos(exp(-factorhub))
-    F = Ftip * Fhub
+    # hub/tip loss
+    F = tiplossfactor(rotor.tipC, r, Rhub, Rtip, phi, B)   
 
     # sec parameters
     k = cn*sigma_p/(4.0*F*sphi*sphi)
